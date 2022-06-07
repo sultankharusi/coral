23dfa0513f35430efcb6149688cbf2c057cab761
sultankharusi <sultan@artificialhorizon.ai>
2022-06-07 17:33:07 +0400

    rewrite whole code

--- reciever_ip.py
+++ reciever_ip.py
@@ -1,16 +1,21 @@
 import socket
 import tqdm
 import os
+import threading
+
 # device's IP address
 SERVER_HOST = "192.168.100.6"
+SERVER_HOST = socket.gethostbyname(socket.gethostname())
 SERVER_PORT = 5001
 # receive 4096 bytes each time
-BUFFER_SIZE = 4096
+HEADER = 4096
+FORMAT = "utf-8"
 SEPARATOR = "<SEPARATOR>"
+DISCONNECT_MESSAGE = "! DISCONNECT"
 
 # create the server socket
 # TCP socket
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # bind the socket to our local address
 s.bind((SERVER_HOST, SERVER_PORT))
@@ -18,40 +23,29 @@ s.bind((SERVER_HOST, SERVER_PORT))
 # enabling our server to accept connections
 # 5 here is the number of unaccepted connections that
 # the system will allow before refusing new connections
-s.listen(5)
-print("[*] Listening as {}:{}".format(SERVER_HOST, SERVER_PORT))
 
-# accept connection if there is any
-client_socket, address = s.accept() 
-# if below code is executed, that means the sender is connected
-print("[+] {} is connected.".format(address))
+def handle_client(conn, addr):
+    print("[NEW CONNECTION] {} connected".format(addr))
 
-# receive the file infos
-# receive using client socket, not server socket
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
-# remove absolute path if there is
-filename = os.path.basename(filename)
-# convert to integer
-filesize = int(filesize)
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) # This blocks the lines of code
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print("{} {}".format(addr, msg))
 
-# start receiving the file from the socket
-# and writing to the file stream
-progress = tqdm.tqdm(range(filesize), "Receiving {}".format(filename), unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "wb") as f:
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+    conn.close()
+
+def start():
+    s.listen()
+    print("[*] Listening as {}:{}".format(SERVER_HOST, SERVER_PORT))
     while True:
-        # read 1024 bytes from the socket (receive)
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:    
-            # nothing is received
-            # file transmitting is done
-            break
-        # write to the file the bytes we just received
-        f.write(bytes_read)
-        # update the progress bar
-        progress.update(len(bytes_read))
+        conn, addr = s.accept() # This blocks the lines of code
+        thread = threading.Tread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print("[ACTIVE CONNECTIONS] {}".format(threading.activeCount() -1))
 
-# close the client socket
-client_socket.close()
-# close the server socket
-s.close()
+print("Firing up server !")
+start()
